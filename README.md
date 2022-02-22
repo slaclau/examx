16f9dc3b70b768aab71bf6cf237a2cfb102adb45
slaclau <seb.laclau@gmail.com>
2022-02-22 16:48:52 +0000

    Cleanup

---
+++ README.md
@@ -0,0 +1,7 @@
+This is version 1.0.0 of the articletobook package, dated 2022/02/11.
+
+This package converts article based classes to book based classes.
+
+This work may be distributed and/or modified under the conditions of
+the LaTeX Project Public License, either version 1.3c of this
+license or (at your option) any later version.
---
+++ build.lua
@@ -0,0 +1,12 @@
+module = "articletobook"
+maindir = "."
+sourcefiles = {"*.dtx", "*.ins", "ltxdoc-commands.def"}
+packtdszip = true
+uploadconfig = {
+  pkg         = "articletobook",
+  author      = "Sebastien Laclau",
+  license     = "lppl1.3c",
+  ctanPath    = "/macros/latex/contrib/articletobook",
+  repository  = "https://github.com/slaclau/articletbook/",
+  summary     = "Converts article based classes to book based classes.",
+}
