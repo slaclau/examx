40a0b1f4c5fd9336c2ba78f3298f23a67dd988b2
slaclau <seb.laclau@gmail.com>
2022-05-15 01:16:10 +0100

    build(build.lua): added examx-formulae

--- build.lua
+++ build.lua
@@ -1,7 +1,9 @@
 module = "examx"
 maindir = "."
-installfiles = {"*.sty", "*.cls", "*.clo", "*tikzconfigforworkbook.tex"}
-sourcefiles = {"*.dtx", "*.ins", "examx-ltxdoc-commands.def", "tikzconfigforworkbook.tex"}
+installfiles = {"*.sty", "*.cls", "*.clo",
+"*tikzconfigforworkbook.tex","examx-formulae.def"}
+sourcefiles = {"*.dtx", "*.ins", "examx-ltxdoc-commands.def",
+"examx-formulae.def", "tikzconfigforworkbook.tex"}
 typesetfiles = {"*.dtx", "examxdoc.tex"}
 
 checkengines = "xetex"
